8ed3fadc729a29fae732c4eed3957c00d23b90e5
codymac <codypetrie89@gmail.com>
2019-03-12 23:32:10 -0700

    I worked on the alpha part of the diss again. I also converted the plotgofr_compare.py file to use numpy arrays. It's much easier to read in data using loadtxt() that way. The code can now also can select every nth data point to use for the continuum, since it's really noisy.

--- results/cluster/gofrnp/fine/plotgofr_compare.py
+++ results/cluster/gofrnp/fine/plotgofr_compare.py
@@ -1,4 +1,5 @@
-import numpy as np
+from numpy import *
+#import numpy as np
 import matplotlib.pyplot as plt
 
 #set up input parameters
@@ -8,9 +9,11 @@ sys=5 #np0=1, np1=3, pp=5, nn=7
 #filenames=('temp.dmc','gofrnp_0.00025_lin_alpha.dmc','gofrnp_0.00025_ip_alpha.dmc','gofrnp_0.0005_lin.dmc','gofrnp_0.0005_ip.dmc')
 #titles=('Continuum Alpha','0.00025 lin Alpha','0.00025 ip Alpha','0.00025 lin Cluster','0.00025 ip Cluster')
 #pltstyle=('ko','ro','mo','yo','go','bo')
-filenames=('gofrnp_0.00025_ip_fine.dmc','gofrnp_0.00025_lin_fine.dmc','../gofrnp_continuum_ip.dmc')
-titles=('0.00025 ip','0.00025 lin','alpha continuum')
-pltstyle=('ko','ro','mo','bo')
+filenames=('../gofrnp_continuum_ip.dmc','gofrnp_0.00025_lin_fine.dmc','gofrnp_0.00025_ip_fine.dmc')
+titles=('Alpha Continuum','Linear - 0.00025','IP - 0.00025')
+#filenames=('../gofrnp_continuum_ip.dmc','gofrnp_0.00025_lin_alpha_fine.dmc','gofrnp_0.00025_ip_alpha_fine.dmc')
+#titles=('Alpha Continuum','Linear 2n2p - 0.00025','IP 2n2p - 0.00025')
+pltstyle=('ko','ro','bo')
 
 #build other parameters
 lenfiles=len(filenames)
@@ -49,49 +52,46 @@ else:
    print ''
    exit()
 
-data=[None]*lenfiles
+data=loadtxt(filenames[0])
 for n in range(0,lenfiles):
-   #build data and remove unwanted pieces
-   f=open(filenames[n])
-   data[n]=[]
-   for line in f.readlines():
-      data[n].append(line.split())
-   f.close()
-   for i in range(0,3): #delete header information
-      del data[n][0]
-   length=len(data[n])-2
-   for i in range(length,length+2): #delete empty two rows at the end
-      del data[n][length]
-   for i in range(0,length):
-      for j in range(0,9): #9 is the number of systems + r + errors for each system
-         data[n][i][j]=float(data[n][i][j])
+   data=array(loadtxt(filenames[n]))
+#   print data[:,0]
+#   data[:,0] = (data[:,0])[0::2]
+#   data[:,0] = delete(data[:,0], arange(0, data[:,0].size, 2))
+#   print ''
+#   print data[:,0]
+#   stop
+
    #normalize data to have unit integral
    norm=0.0
+   length=len(data)
    for i in range(0,length):
-      norm=norm+data[n][i][sys]*(data[n][1][0]-data[n][0][0])
+      norm=norm+data[i,sys]*(data[1,0]-data[0,0])
    for i in range(0,length):
-      data[n][i][sys]=data[n][i][sys]/norm
-      data[n][i][sys+1]=data[n][i][sys+1]/norm
-   data[n]=zip(*data[n])
+      data[i,sys]=data[i,sys]/norm
+      data[i,sys+1]=data[i,sys+1]/norm
 
-f1=plt.figure(1)
-plt.rcParams.update({'font.size': 15})
-plt.xlim([0,10])
-plt.xlabel('r(fm)')
-plt.ylabel(r'g$_{'+sysname+'}(r)$')
-plt.axhline(linewidth=2, color='k')
-for n in range(0,lenfiles):
-   plt.errorbar(data[n][:][0],data[n][:][sys],yerr=data[n][:][sys+1],fmt=pltstyle[n],label=titles[n])
-plt.legend(loc='upper right',numpoints=1)
+   #Plot the data
+   f1=plt.figure(1)
+   plt.rcParams.update({'font.size': 15})
+   plt.xlim([0,10])
+   plt.xlabel('r(fm)')
+   plt.ylabel(r'g$_{'+sysname+'}(r)$')
+   plt.axhline(linewidth=2, color='k')
+   if(n==0):
+      plt.errorbar((data[:,0])[0::3],(data[:,sys])[0::3],yerr=(data[:,sys+1])[0::3],fmt=pltstyle[n],label=titles[n])
+   else:
+      plt.errorbar(data[:,0],data[:,sys],yerr=data[:,sys+1],fmt=pltstyle[n],label=titles[n])
+   plt.legend(loc='upper right',numpoints=1)
+
+#   f2=plt.figure(2)
+#   plt.xlim([4,10])
+#   plt.ylim([0,0.05])
+#   plt.xlabel('r(fm)')
+#   plt.ylabel(r'g$_{'+sysname+'}(r)$')
+#   plt.errorbar(data[n][:][0],data[n][:][sys],yerr=data[n][:][sys+1],fmt=pltstyle[n],label=titles[n])
+#   plt.legend(loc='upper right',numpoints=1)
+
 f1.savefig("plot1.pdf", bbox_inches='tight')
-
-f2=plt.figure(2)
-plt.xlim([4,10])
-plt.ylim([0,0.05])
-plt.xlabel('r(fm)')
-plt.ylabel(r'g$_{'+sysname+'}(r)$')
-for n in range(0,lenfiles):
-   plt.errorbar(data[n][:][0],data[n][:][sys],yerr=data[n][:][sys+1],fmt=pltstyle[n],label=titles[n])
-plt.legend(loc='upper right',numpoints=1)
-f2.savefig("plot2.pdf", bbox_inches='tight')
+#f2.savefig("plot2.pdf", bbox_inches='tight')
 plt.show()
