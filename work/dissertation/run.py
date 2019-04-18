33e001ede0b6ac69ae9246fa089ad243378f1ca7
Cody Petrie <clpetrie@asu.edu>
2019-04-18 16:48:43 -0700

    I have added a bit about ChiEFT to my dissertation. I am not adding Diego's calculations with ChiEFT potentials and the quadratic correlations.

--- work/dissertation/run.py
+++ work/dissertation/run.py
@@ -1,10 +1,14 @@
 import os
 import platform
+import sys
 
-os.system('pdflatex dis.tex')
-os.system('bibtex dis')
-os.system('pdflatex dis.tex')
-os.system('pdflatex dis.tex')
+if(len(sys.argv) == 1):
+   os.system('pdflatex dis.tex')
+   os.system('bibtex dis')
+   os.system('pdflatex dis.tex')
+   os.system('pdflatex dis.tex')
+else:
+   os.system('pdflatex dis.tex')
 if platform.system() == 'Darwin':
    os.system('open dis.pdf')
 if platform.system() == 'Linux':
